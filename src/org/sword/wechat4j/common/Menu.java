08dd61c16b062ceb3a36390ff37148999b4235f9
chengn <cn2005nian@163.com>
2015-05-11 17:43:00 +0800

    菜单操作框架

---
+++ src/org/sword/wechat4j/common/Menu.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package org.sword.wechat4j.common;
+
+/**
+ * 微信菜单操作
+ * @author chengn
+ * @date   2015年5月11日
+ */
+public class Menu {
+	
+	/**
+	 * 创建
+	 */
+	public void create(){
+		
+	}
+	
+	/**
+	 * 更新
+	 */
+	public void update(){
+		
+	}
+	
+	/**
+	 * 删除
+	 */
+	public void delete(){
+		
+	}
+}
